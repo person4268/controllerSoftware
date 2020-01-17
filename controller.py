8c528040f10c0faf9b73c9e53d0c5d7b52dbd6ad
person4268 <person4268@person4269.cf>
2020-01-17 15:25:54 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,18 +1,84 @@
+# As much as I hate windows, this is windows only
+# Mostly because I couldn't find a way to get games to pick up the inputs
+
 import serial
 import sys
 import io
 
+import os
+import ctypes
+import win32api
 
-controller = serial.Serial("/dev/ttyUSB0")
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+   _fields_ = [("wVk", ctypes.c_ushort),
+               ("wScan", ctypes.c_ushort),
+               ("dwFlags", ctypes.c_ulong),
+               ("time", ctypes.c_ulong),
+               ("dwExtraInfo", PUL)]
+
+
+class HardwareInput(ctypes.Structure):
+   _fields_ = [("uMsg", ctypes.c_ulong),
+               ("wParamL", ctypes.c_short),
+               ("wParamH", ctypes.c_ushort)]
+
+
+class MouseInput(ctypes.Structure):
+   _fields_ = [("dx", ctypes.c_long),
+               ("dy", ctypes.c_long),
+               ("mouseData", ctypes.c_ulong),
+               ("dwFlags", ctypes.c_ulong),
+               ("time", ctypes.c_ulong),
+               ("dwExtraInfo", PUL)]
+
+
+class Input_I(ctypes.Union):
+   _fields_ = [("ki", KeyBdInput),
+               ("mi", MouseInput),
+               ("hi", HardwareInput)]
+
+
+class Input(ctypes.Structure):
+   _fields_ = [("type", ctypes.c_ulong),
+("ii", Input_I)]
+
+def press_key(key):
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+
+   flags = 0x0008
+
+   ii_.ki = KeyBdInput(0, key, flags, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(1), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def release_key(key):
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+
+   flags = 0x0008 | 0x0002
+
+   ii_.ki = KeyBdInput(0, key, flags, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(1), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+
+controller = serial.Serial("COM4")
 controller.baudrate = 9600
+
+KEY_U = 0x16
+KEY_D = 0x20
 
 while True:
 	input = controller.read(1)
 	if input == b"U":
-		keyboard.press_key("u")
+		press_key(KEY_U)
 	elif input == b"u":
-		keyboard.release_key("u")
+		release_key(KEY_U)
 	elif input == b"D":
-		keyboard.press_key("d")
+		press_key(KEY_D)
 	elif input == b"d":
-		keyboard.release_key("d")
+		release_key(KEY_D)

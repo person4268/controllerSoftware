152f3023e276e07c614aedb0e66440dc0527f657
person4268 <person4268@person4269.cf>
2020-01-20 15:05:47 -0500

    Add slelect + start button support

--- controller.py
+++ controller.py
@@ -64,9 +64,10 @@ def release_key(key):
    x = Input(ctypes.c_ulong(1), ii_)
    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
+def debug_print(str):
+   print(str) #comment out for no debug
 
-
-controller = serial.Serial("COM4")
+controller = serial.Serial("COM3")
 controller.baudrate = 9600
 
 KEY_U = 0x16
@@ -78,35 +79,55 @@ KEY_E = 0x12
 KEY_R = 0x13
 KEY_T = 0x14
 
+KEY_K = 0x25
+KEY_L = 0x26
+
 while True:
    input = controller.read(1)
    if input == b"U":
       press_key(KEY_U)
+      debug_print("[PRESS]: U")
    elif input == b"u":
       release_key(KEY_U)
    elif input == b"D":
       press_key(KEY_D)
+      debug_print("[PRESS]: D")
    elif input == b"d":
       release_key(KEY_D)
    elif input == b"Q":
       press_key(KEY_Q)
+      debug_print("[PRESS]: Q")
    elif input == b"q":
       release_key(KEY_Q)
    elif input == b"W":
       press_key(KEY_W)
+      debug_print("[PRESS]: W")
    elif input == b"w":
       release_key(KEY_W)
    elif input == b"E":
       press_key(KEY_E)
+      debug_print("[PRESS]: E")
    elif input == b"e":
       release_key(KEY_E)
    elif input == b"R":
       press_key(KEY_R)
+      debug_print("[PRESS]: R")
    elif input == b"r":
       release_key(KEY_R)
    elif input == b"T":
       press_key(KEY_T)
+      debug_print("[PRESS]: T")
    elif input == b"t":
       release_key(KEY_T)
+   elif input == b"K":
+      press_key(KEY_K)
+      debug_print("[PRESS]: K")
+   elif input == b"k":
+      release_key(KEY_K)
+   elif input == b"L":
+      press_key(KEY_L)
+      debug_print("[PRESS]: L")
+   elif input == b"l":
+      release_key(KEY_L)
    time.sleep(0.010)
    
